ffcc8d0b62a796194030075db06c036f69510d40
mohammadhossein <83216842+motefaker@users.noreply.github.com>
2022-09-10 12:03:24 +0430

    Update miniCrawler.py

--- miniCrawler.py
+++ miniCrawler.py
@@ -1,19 +1,48 @@
 import requests
 from bs4 import BeautifulSoup
 from bs4 import Comment
+import asyncio
+from pyppeteer import launch
+import os
 class Crawler():
     url ='https://www.w3schools.com/python/'
 
     def __init__(self) -> None:
         pass
 
+    async def startProgram2(self):
+        #driver = webdriver.Chrome()
+        #driver.get(self.url)
+        # rawData =requests.get(self.url)  
+        # soup = BeautifulSoup(rawData.content, "html.parser")
+        # self.__choose_subject(soup)  
+
+         # Launch the browser
+        browser = await launch()
+
+        # Open a new browser page
+        page = await browser.newPage()
+
+        # Create a URI for our test file
+        page_path = "https://www.digikala.com/search/category-groceries/"
+
+        # Open our test file in the opened page
+        await page.goto(page_path)
+        page_content = await page.content()
+
+        # Process extracted content with BeautifulSoup
+        soup = BeautifulSoup(page_content)
+        print(page_content)
+
+        # Close browser
+        await browser.close()
+
+
     def startProgram(self):
         #driver = webdriver.Chrome()
         #driver.get(self.url)
         rawData =requests.get(self.url)  
-        soup = BeautifulSoup(rawData.content, "html.parser")
-        
-               
+        soup = BeautifulSoup(rawData.content, "html.parser")      
         self.__choose_subject(soup)
 
     def __choose_subject(self, soup):
@@ -32,29 +61,56 @@ class Crawler():
         temp=main_topics[chosennumber-1]
         myList = []
         while(True):
-           if temp.nextSibling !=  main_topics[chosennumber]:
+           if temp !=  main_topics[chosennumber]:
                 if isinstance(temp,Comment):
                     #temp2 = temp
                     temp= temp.nextSibling
                     #temp2.extract()
                     continue
                 #if boz.name !="div":
-                if temp.text!= "\n" :
+                if temp.text!= "\n" and  temp.name !="br":
                     myList.append(temp) 
                 temp =temp.nextSibling
            else :
                 break 
-
+        count =1        
         for name in myList:
             if  name.name != "div" :
-                print (name.text)        
+                print (str(count) +":"+name.text)   
+                count+=1     
         chosennumber =self.inputManager(len(myList),"choose your inner topic plz:  ")
-        if myList[chosennumber-1].name != "div":
-           newurl =self.url+ str(myList[chosennumber-1]['href'])
-           print(newurl)
+        chosenObject = self.translateChosenNumber(list=myList,number=chosennumber-1)
+        
+        if chosenObject.name != "div":
+           newurl =self.url+ str(chosenObject['href'])
+           
         else:
-           self.__innerTopics(myList[chosennumber-1])   
+           self.__innerTopics(chosenObject)   
+    def extractMainPage(self,url):
+        rawData =requests.get(url)  
+        soup = BeautifulSoup(rawData.content, "html.parser")      
+        soup.find_all(name = "a", string="Try it Yourself »",target="_blank")
 
+        pass
+
+
+
+
+
+
+
+
+    def translateChosenNumber(self,number,list):
+        newList = []
+        for i  in list:
+            if i.name != "div":
+                newList.append(i)
+
+        count = list.index(newList[number])
+        if count == len(list)-1 or list[count+1].name !="div":
+            return list[count]
+        elif list[count+1].name =="div":
+            return list[count+1]    
 
 
 
@@ -81,4 +137,5 @@ class Crawler():
 
 
 crawler = Crawler()
+
 crawler.startProgram()
